12905947aa25a960ba357989880476ee3e503023
Kajetan <40916030+Telchar22@users.noreply.github.com>
2020-09-28 21:22:09 +0200

    Update Greatest Number of.py

--- Python/Greatest Number of.py
+++ Python/Greatest Number of.py
@@ -1,11 +1,12 @@
 from typing import List
 
 class Solution:
-    def isGreatestNumberPossible(items: List[int], extras: int) -> List[bool]:
+    def isGreatestNumberPossible(self, items: List[int], extras: int) -> List[bool]:
         ans = [True if(items[i]+extras >= max(items)) else False for i in range(len(items))]
         return ans
 
 
 items = [2, 3, 5, 1, 3]
 extras = 3
-print(Solution.isGreatestNumberPossible(items, extras))
+s = Solution()
+print(s.isGreatestNumberPossible(items, extras))
