4a71476f9dab6abcbcc9cfad1e7f6808dca383fe
Kajetan <40916030+Telchar22@users.noreply.github.com>
2020-09-30 14:35:47 +0200

    Update DefangIP.py

--- Python/DefangIP.py
+++ Python/DefangIP.py
@@ -1,7 +1,7 @@
 #change . to [.]
 class Solution:
     def defangIPaddr(self, address: str) -> str:
-        ads_list = list(address) # str cant be changed in Python so for manipulations we change it to list
+        ads_list = list(address) 
         ads_list = ["[.]" if (ads_list[i]== ".") else ads_list[i] for i in range(len(ads_list))]
         address = "".join(ads_list)
         return address
